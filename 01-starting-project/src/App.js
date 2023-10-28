dfbeab469a6260d57890517c9854be6837b6ed14
Tarburt <100755979+Tarburt@users.noreply.github.com>
2023-10-28 05:51:33 +0000

    reloading again and again

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -4,25 +4,58 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  // usf
   const [movies, setMovies] = useState([]);
-  const [isLoading, setisLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [retryTimeoutId, setRetryTimeoutId] = useState(null);
 
   async function fetchMoviesHandler() {
-    setisLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    // .json to convert data into object like structure
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
-    setisLoading(false);
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/film/");
+      // response.ok एक method है जो देखता है कि response में कोई गलती है या नहीं। 
+      // अगर कोई गलती नहीं है, तो यह true return करता है, नहीं तो false
+      if (!response.ok) {
+        throw new Error("Something went wrong ....Retrying");
+      }
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+      // time function call किए जाने पर ID देता है। clearTimeout(ID) को कॉल करके function को हटाया जा सकता है।
+      setRetryTimeoutId(setTimeout(fetchMoviesHandler, 5000));
+    }
+    setIsLoading(false);
+  }
+
+  function cancelRetryHandler() {
+    clearTimeout(retryTimeoutId); // Cancel the retry
+    setRetryTimeoutId(null); // Clear the timeout ID
+  }
+
+  let content = <p>Found no movies.</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = (
+      <div>
+        <p>{error}</p>
+        <button onClick={cancelRetryHandler}>Cancel Retry</button>
+      </div>
+    );
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -30,11 +63,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>found no movies</p>}
-        {isLoading && <p>Loading ...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
